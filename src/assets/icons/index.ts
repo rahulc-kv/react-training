b87b9193e2b417d170e92d2b3c5958ba0bf894a6
anjuca154 <anjuca154@gmail.com>
2022-07-12 18:19:09 +0530

    Final edit

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,19 +1,8 @@
 import { ReactComponent as RefreshIcon } from './refresh.svg';
 import { ReactComponent as MoreIcon } from './more.svg';
-import { ReactComponent as DotIcon } from './dot.svg';
 import { ReactComponent as CoinBase } from './coinbase.svg';
 import { ReactComponent as Celsius } from './celsius.svg';
-import { ReactComponent as Inner } from './inner.svg';
 import { ReactComponent as Gemini } from './gemini.svg';
 import { ReactComponent as Binance } from './binance.svg';
 
-export {
-  RefreshIcon,
-  MoreIcon,
-  DotIcon,
-  CoinBase,
-  Celsius,
-  Inner,
-  Gemini,
-  Binance
-};
+export { RefreshIcon, MoreIcon, CoinBase, Celsius, Gemini, Binance };
