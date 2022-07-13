c1820ca0780f0fa2ebe5dd769013f541a6f50656
Cizil <cizilmaria@gmail.com>
2022-07-13 09:57:04 +0530

    Resolved

--- src/containers/files-listing/FilesList.tsx
+++ src/containers/files-listing/FilesList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Cards from '@components/Cards';
 
 const FilesList = () => {
@@ -7,7 +8,7 @@ const FilesList = () => {
       <p className="pt-5 pl-10 text-blue-600 underline underline-offset-1">
         Upload a generic File
       </p>
-      <Cards></Cards>
+      <Cards />
     </div>
   );
 };
