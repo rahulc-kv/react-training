634011d6bdb9a438a19f848fbba9c88a876feb20
cizil99 <109058674+cizil99@users.noreply.github.com>
2022-07-13 11:48:37 +0530

    Cards Component (#2)
    * Cards created
    
    * Cards mapped
    
    * Changes made
    
    * Changes made
    
    * Resolved
    
    * Changes made
    
    * Merged developed into f-cards
    
    Co-authored-by: Cizil <cizilmaria@gmail.com>

---
+++ src/components/Cards.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+
+import { Binance } from '/home/cizil/Practise/cards/react-training/src/assets/index';
+import { Celsius } from '/home/cizil/Practise/cards/react-training/src/assets/index';
+import { Coinbase } from '/home/cizil/Practise/cards/react-training/src/assets/index';
+import { Gemini } from '/home/cizil/Practise/cards/react-training/src/assets/index';
+
+const Cards = () => {
+  const files = [
+    {
+      id: 1,
+      img: Binance,
+      Linked: true,
+      isActive: true
+    },
+    {
+      id: 2,
+      img: Binance,
+      Linked: false,
+      isActive: true
+    },
+    {
+      id: 3,
+      img: Coinbase,
+      Linked: false,
+      isActive: true
+    },
+    {
+      id: 4,
+      img: Coinbase,
+      Linked: false,
+      isActive: true
+    },
+    {
+      id: 5,
+      img: Celsius,
+      Linked: true,
+      isActive: true
+    },
+    {
+      id: 6,
+      img: Celsius,
+      Linked: false,
+      isActive: true
+    },
+    {
+      id: 7,
+      img: Gemini,
+      Linked: false,
+      isActive: true
+    },
+    {
+      id: 8,
+      img: Gemini,
+      Linked: false,
+      isActive: true
+    },
+    {
+      id: 9,
+      img: Binance,
+      Linked: true,
+      isActive: true
+    },
+    {
+      id: 10,
+      img: Binance,
+      Linked: false,
+      isActive: false
+    }
+  ];
+
+  return (
+    <div>
+      <div className=" flex flex-wrap gap-5 pt-10 pl-10 ">
+        {files.map(file => {
+          const Img = file.img;
+
+          return (
+            <div
+              key={file.id}
+              className=" flex flex-col flex-nowrap items-center  w-64 h-24 bg-white rounded-xl">
+              <Img className="  w-32 h-24" />
+
+              <div>
+                <div
+                  className={`w-60  text-xs text-center text-emerald-500 no-underline 
+                    hover:underline bg-slate-100 rounded-b-md ${
+                      file.Linked ? 'visible' : 'invisible'
+                    }`}>
+                  Linked via api
+                </div>
+              </div>
+
+              {!file.isActive && (
+                <div
+                  className=" w-60 text-xs text-center text-sky-700 no-underline 
+                  hover:underline bg-slate-100  rounded-b-md">
+                  Coming soon
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
